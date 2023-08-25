87222d3e44839828235802360147e78e14e2d6e0
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-08-25 20:33:08 +0530

    Update & Rename src.main

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from src.components.fetch import response
 from src.database.operation import DataFrameOperations
 from src.entity import Acres99
 
-MAX_PAGE_NUM = 1
+PAGES_LIST = [1, 2, 3]
 MAX_PAGE_SIZE = 30
 
 
@@ -17,11 +17,13 @@ async def process_request(page_num: int, page_size: int):
         acres = Acres99(**acres99_dict)
         df_operation = DataFrameOperations(acres)
         await df_operation.export_dfs(drop_duplicates=True)
+    else:
+        raise RuntimeError('Response data is not dict data type.')
 
 
 async def main():
     tasks = []
-    for i in range(0, MAX_PAGE_NUM):
+    for i in PAGES_LIST:
         task = process_request(i, MAX_PAGE_SIZE)
         tasks.append(task)
     await asyncio.gather(*tasks)
