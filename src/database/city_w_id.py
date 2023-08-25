0852d64a3c9ef43327d667c0ad141e20d3aa07f7
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-08-26 01:43:01 +0530

    Add src.database.city_w_id

---
+++ src/database/city_w_id.py
@@ -0,0 +1,36 @@
+import json
+from pathlib import Path
+from typing import Any
+
+import requests
+
+from src import utils
+
+CITY_W_ID_PATH = Path('data/city_w_id.json')
+
+
+def fetch_city_w_id_data() -> dict[str, Any]:
+    r = requests.get(
+        url='https://www.99acres.com/api-aggregator/content/locations/group/PROPERTY_CITY?rtype=json',
+        headers=utils.request_headers,
+    )
+    r.raise_for_status()
+    return r.json()
+
+
+def get_city_w_id_dict() -> dict[int, str]:
+    data: list[dict[str, str]] = fetch_city_w_id_data()['locationDropDowns']
+
+    ids = [int(i['locationId'].split('_')[0]) for i in data]
+    cities = [i['label'] for i in data]
+
+    return dict(zip(ids, cities))
+
+
+def save_city_w_id_dict() -> None:
+    with open(CITY_W_ID_PATH, 'w') as f:
+        json.dump(get_city_w_id_dict(), f, indent=2, sort_keys=True)
+
+
+if __name__ == '__main__':
+    save_city_w_id_dict()
---
+++ src/utils.py
@@ -0,0 +1,3 @@
+request_headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:101.0) Gecko/20100101 Firefox/114.0"
+}
