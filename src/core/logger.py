3f4ae4a8bd135e58f887f11b0536a96a199d6c2d
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-08-25 17:34:52 +0530

    Refactor src.core.logger

--- src/core/logger.py
+++ src/core/logger.py
@@ -1,6 +1,4 @@
 import logging
-from dataclasses import dataclass
-from datetime import datetime as dt
 from pathlib import Path
 
 
@@ -8,38 +6,20 @@ def get_logger(logger_name: str) -> logging.Logger:
     """
     :logger_name (str): `__name__`
 
-    :returns: Logger
+    :returns: logging.Logger
     """
-    return Logger(logger_name).get_logger
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
 
+    fp = Path('logs/Acres99.log')
+    fp.parent.mkdir(parents=True, exist_ok=True)
 
-@dataclass
-class Logger:
-    """
-    Logger for the project.
+    formatter = logging.Formatter(
+        "[%(asctime)s]:%(levelname)s - [%(lineno)d]:%(name)s - %(message)s"
+    )
 
-    Args:
-        logger_name: __name__
-    """
+    file_handler = logging.FileHandler(fp, mode='w')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
 
-    logger_name: str
-
-    def __post_init__(self):
-        self.logger = logging.getLogger(self.logger_name)
-        self.logger.setLevel(logging.DEBUG)
-
-        fp = Path(f'logs/{dt.now():%d%m%y_%H%M}.log')
-        fp.parent.mkdir(parents=True, exist_ok=True)
-
-        formatter = logging.Formatter(
-            "[ %(asctime)s ] %(filename)s:[%(lineno)d] - %(name)s - %(levelname)s - %(message)s"
-        )
-
-        file_handler = logging.FileHandler(fp)
-        file_handler.setFormatter(formatter)
-        self.logger.addHandler(file_handler)
-
-    @property
-    def get_logger(self) -> logging.Logger:
-        """ Get the Logger object. """
-        return self.logger
+    return logger
