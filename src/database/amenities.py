42d4c46841710b2461e0e03edf175a3aea9aad74
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-08-30 19:48:06 +0530

    Add src.database.amenities

---
+++ src/database/amenities.py
@@ -0,0 +1,43 @@
+from typing import Any
+
+import pandas as pd
+import requests
+
+from src import utils
+from src.core.logger import get_logger
+
+amenities_fp = 'data/facets/AMENITIES.csv'
+
+logger = get_logger(__name__)
+
+
+def fetch_amenities_data() -> dict[str, Any]:
+    r = requests.get(
+        url='https://www.99acres.com/api-aggregator/static-attributes?includedFields=',
+        headers=utils.request_headers,
+    )
+    logger.info(f"Status[{r.status_code}]:{r.url}")
+
+    r.raise_for_status()
+    return r.json()['propertyAmenity']
+
+
+def amenities_to_csv() -> None:
+    amenities = fetch_amenities_data()
+
+    df = pd.DataFrame([j for i in amenities for j in amenities[i]['default']])
+    df.sort_values(by=['category', 'id'], inplace=True)
+
+    duplicated_idx = df[df['id'].duplicated()].index
+    df.drop(columns=['url'], index=duplicated_idx, inplace=True)
+
+    df.to_csv(amenities_fp, index=False)
+    logger.info(f'Export: "{amenities_fp}"')
+
+
+def main():
+    amenities_to_csv()
+
+
+if __name__ == '__main__':
+    main()
