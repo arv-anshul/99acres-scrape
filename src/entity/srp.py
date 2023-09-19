9cc5cda4507d787d2b0dc167c173be68e2eef077
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-09-19 14:00:48 +0530

    🔨 Update SRPEntity features

--- src/entity/srp.py
+++ src/entity/srp.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Optional
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -10,23 +10,15 @@ class MAPDETAILS(BaseModel):
     LONGITUDE: str
 
 
-class FORMATTEDLANDMARKDETAIL(BaseModel):
-    text: str
-
-
 class Location(BaseModel):
     CITY: str
     CITY_NAME: str
-    BUILDING_ID: str
-    BUILDING_NAME: str
-    SOCIETY_NAME: str
     LOCALITY_ID: str
     LOCALITY_NAME: str
     ADDRESS: Optional[str] = None
 
 
 class SRPEntity(BaseModel):
-    SPID: str
     PROP_ID: str
     PREFERENCE: str
     DESCRIPTION: str
@@ -41,46 +33,26 @@ class SRPEntity(BaseModel):
     AGE: str
     TOTAL_FLOOR: Optional[str] = None
     FEATURES: str
-    REGISTER_DATE: str
-    POSTING_DATE: Optional[int] = None
-    UPDATE_DATE: int
-    CLASS: str
-    RES_COM: str
     PROP_NAME: str
-    PROPERTY_NUMBER: str
-    MIN_PRICE: str
-    MAX_PRICE: str
     PRICE_SQFT: str
-    PROJ_ID: str
-    BUILDING_ID: str
-    VERIFIED: str
     MAP_DETAILS: MAPDETAILS
-    MIN_AREA_SQFT: str
-    MAX_AREA_SQFT: str
-    AMENITIES: str
-    ALT_TAG: Optional[str] = None
-    PRODUCT_TYPE: str
-    TOP_USPS: Optional[List[str]] = None
-    EXPIRY_DATE: str
-    PROPERTY_TYPE__U: str
+    AMENITIES: Optional[str] = None
     AREA: str
-    SECONDARY_AREA: str
     PRICE: str
     PROP_HEADING: str
-    VALUE_LABEL: str
-    CLASS_HEADING: str
-    CLASS_LABEL: str
-    REGISTER_DATE__U: str
-    REGISTERED_DAYS: str
-    PRIMARY_TAGS: List
-    SECONDARY_TAGS: List[str]
+    SECONDARY_TAGS: list[str]
     TOTAL_LANDMARK_COUNT: Optional[int] = None
-    FORMATTED_LANDMARK_DETAILS: Optional[List[FORMATTEDLANDMARKDETAIL]] = None
-    SOCIETY_NAME: str
-    BUILDING_NAME: str
+    FORMATTED_LANDMARK_DETAILS: Optional[list[dict[str, str]]] = None
+    SOCIETY_NAME: Optional[str] = None
+    BUILDING_NAME: Optional[str] = None
     location: Location
     BALCONY_NUM: Optional[str] = None
     FLOOR_NUM: Optional[str] = None
+    CARPET_SQFT: Optional[int] = None
+    SUPERBUILTUP_SQFT: Optional[int] = None
+    BUILTUP_SQFT: Optional[int] = None
+    SUPER_AREA: Optional[int] = None
+    SUPERAREA_UNIT: Optional[str] = None
 
     @property
     def get_attrs(self) -> list[str]:
