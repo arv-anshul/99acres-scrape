ee794bc0b5ab057642f137166d3a2b1726dbd2c8
Anshul Raj Verma <111767754+arv-anshul@users.noreply.github.com>
2023-09-24 20:55:10 +0530

    ♻️ Refactor src.component.fetch

--- src/components/fetch.py
+++ src/components/fetch.py
@@ -2,12 +2,9 @@ import json
 from pathlib import Path
 from typing import Any
 
-import aiohttp
+import httpx
 
 from src.core.logger import get_logger
-from src.typing import Acres99Dict
-
-from . import convert
 
 logger = get_logger(__name__)
 
@@ -16,7 +13,7 @@ REQUESTS_PATH = Path('requests.json')
 
 def get_requests_json() -> dict[str, Any]:
     if not REQUESTS_PATH.exists():
-        raise FileNotFoundError(f'No requests JSON file exists. Make one at "{REQUESTS_PATH}".')
+        raise FileNotFoundError(f'No `requests.json` file exists. Make one at "{REQUESTS_PATH}".')
     return json.load(open(REQUESTS_PATH))
 
 
@@ -25,34 +22,32 @@ def update_url_params(
     page_num: int,
     prop_per_page: int,
 ) -> dict[str, str]:
-    if prop_per_page > 1200:
-        raise ValueError('page_size <= 1200')
+    if prop_per_page > 800:
+        raise ValueError(f'page_size <= {prop_per_page}')
     params_dict['page'] = str(page_num)
     params_dict['page_size'] = str(prop_per_page)
     return params_dict
 
 
 async def fetch_response(
-    session: aiohttp.ClientSession,
+    session: httpx.AsyncClient,
     **kwargs,
-) -> Acres99Dict:
+) -> dict:
     try:
-        async with session.get(**kwargs, timeout=3) as r:
-            msg = 'Status[%s]: %s' % (r.status, r.url)
-            logger.info(msg)
+        r = await session.get(**kwargs, timeout=3)
+        msg = 'Status[%s]: %s' % (r.status_code, r.url)
+        logger.info(msg)
 
-            if r.status > 200:
-                logger.exception(msg)
-                raise aiohttp.ClientResponseError(
-                    request_info=r.request_info, history=(r,), status=r.status, message=msg
-                )
-            response = await r.json()
+        if r.status_code > 200:
+            logger.exception(msg)
+            raise httpx.HTTPStatusError(msg, request=r.request, response=r)
 
-        return Acres99Dict(**await convert.convert_to_acres99_dict(response))
+    except httpx.HTTPStatusError as e:
+        logger.exception(f'Error fetching response: {e}')
+        raise e
 
-    except aiohttp.ClientResponseError as cre:
-        logger.exception(f'Error fetching response: {cre}')
-        raise cre
+    else:
+        return r.json()
 
 
 async def fetch_all_responses(
@@ -60,7 +55,7 @@ async def fetch_all_responses(
     prop_per_page: int,
     city_id: int | None = None,
     **kwargs,
-) -> list[Acres99Dict]:
+) -> list[dict]:
     """
     :page_num (int): Page number.
     :page_size (int): No. of properties data.
@@ -69,15 +64,13 @@ async def fetch_all_responses(
     """
     if len(kwargs) == 0:
         kwargs = get_requests_json()
-
     if city_id:
         kwargs['params']['city'] = city_id
 
-    async with aiohttp.ClientSession() as session:
+    async with httpx.AsyncClient() as session:
         responses = []
-
-        for p_num in page_nums:
-            kwargs['params'] = update_url_params(kwargs['params'], p_num, prop_per_page)
+        for page_num in page_nums:
+            kwargs['params'] = update_url_params(kwargs['params'], page_num, prop_per_page)
             responses.append(await fetch_response(session, **kwargs))
 
     return responses
